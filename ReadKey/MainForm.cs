b3da2b038f98ead842811b16e10f258862f51310
FENGberd <fengberd@gmail.com>
2018-12-23 13:32:46 +0800

    uhm

---
+++ ReadKey/MainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Management;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
+
+namespace ReadKey
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+            if(!WinApi.grantPrivilege("SeDebugPrivilege"))
+            {
+                MessageBox.Show("无法提权到SeDebugPrivilege","错误",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+        }
+
+        private void log(string tag,string text)
+        {
+            textBox1.AppendText("[" + tag + "]" + text + Environment.NewLine);
+        }
+
+        private void button2_Click(object sender,EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button3_Click(object sender,EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void button1_Click(object sender,EventArgs e)
+        {
+            foreach(var p in Process.GetProcesses())
+            {
+                string name;
+                try
+                {
+                    name = p.MainModule.FileName.ToLower();
+                }
+                catch
+                {
+                    continue;
+                }
+                if(name.EndsWith("pycplayer.exe"))
+                {
+                    IntPtr process = WinApi.OpenProcess(WinApi.ProcessAccessFlags.VirtualMemoryRead | WinApi.ProcessAccessFlags.QueryInformation,false,p.Id);
+                    if(process == IntPtr.Zero)
+                    {
+                        log("ERROR/" + p.Id,"OpenProcess failed(" + Marshal.GetLastWin32Error() + ").");
+                        continue;
+                    }
+                    if(WinApi.EnumProcessModules(process,out uint module,8,out uint cbNeeded) == 0)
+                    {
+                        log("ERROR/" + p.Id,"EnumProcessModules failed(" + Marshal.GetLastWin32Error() + ").");
+                        continue;
+                    }
+                    byte[] vBuffer = new byte[16];
+                    IntPtr vBytesAddress = Marshal.UnsafeAddrOfPinnedArrayElement(vBuffer,0);
+                    WinApi.ReadProcessMemory(process,new IntPtr(module + 0x1152398),vBuffer,vBuffer.Length,out int count);
+                    string tag = "TAG_NOT_EXISTS";
+                    foreach(var c in GetCommandLine(p).Split('"'))
+                    {
+                        if(c.EndsWith(".pbb",StringComparison.OrdinalIgnoreCase))
+                        {
+                            try
+                            {
+                                tag = Path.GetFileNameWithoutExtension(c);
+                                break;
+                            }
+                            catch { }
+                        }
+                    }
+                    log("INFO"," Key from " + p.Id + "(" + tag + ")  " + BitConverter.ToString(vBuffer).Replace("-",string.Empty));
+                }
+            }
+
+        }
+
+        private static string GetCommandLine(Process process)
+        {
+            using(ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+            using(ManagementObjectCollection objects = searcher.Get())
+            {
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+            }
+
+        }
+    }
+}
---
+++ ReadKey/WinApi.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ReadKey
+{
+    public class WinApi
+    {
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [DllImport("kernel32.dll",SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess,IntPtr lpBaseAddress,[Out] byte[] lpBuffer,int dwSize,out int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll",SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess,bool bInheritHandle,int processId);
+
+        [DllImport("kernel32.dll",SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("psapi.dll",CallingConvention = CallingConvention.StdCall,SetLastError = true)]
+        public static extern int EnumProcessModules(IntPtr hProcess,out uint lphModule,uint cb,out uint lpcbNeeded);
+
+        [DllImport("kernel32.dll",CharSet = CharSet.Auto,SetLastError = true)]
+        public static extern IntPtr GetCurrentProcess();
+
+        [DllImport("Advapi32.dll",CharSet = CharSet.Auto,SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool OpenProcessToken(IntPtr ProcessHandle,uint DesiredAccesss,out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll",CharSet = CharSet.Unicode,SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool LookupPrivilegeValue(string lpSystemName,string lpName,[MarshalAs(UnmanagedType.Struct)] ref LUID lpLuid);
+
+        [DllImport("advapi32.dll",CharSet = CharSet.Unicode,SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AdjustTokenPrivileges(IntPtr TokenHandle,[MarshalAs(UnmanagedType.Bool)] bool DisableAllPrivileges,[MarshalAs(UnmanagedType.Struct)]ref TOKEN_PRIVILEGES NewState,uint BufferLength,IntPtr PreviousState,uint ReturnLength);
+        
+        [StructLayout(LayoutKind.Sequential,CharSet = CharSet.Unicode)]
+        public struct LUID
+        {
+            public int LowPart;
+            public uint HighPart;
+        }
+
+        [StructLayout(LayoutKind.Sequential,CharSet = CharSet.Unicode)]
+        public struct LUID_AND_ATTRIBUTES
+        {
+            public LUID Luid;
+            public uint Attributes;
+        }
+
+        [StructLayout(LayoutKind.Sequential,CharSet = CharSet.Unicode)]
+        public struct TOKEN_PRIVILEGES
+        {
+            public int PrivilegeCount;
+            public LUID_AND_ATTRIBUTES Privilege;
+        }
+
+        public static bool grantPrivilege(string name)
+        {
+            LUID privilegeId = new LUID();
+            if(!LookupPrivilegeValue(null,name,ref privilegeId))
+            {
+                return false;
+            }
+            TOKEN_PRIVILEGES tokenPrivileges = new TOKEN_PRIVILEGES
+            {
+                PrivilegeCount = 1,
+                Privilege = new LUID_AND_ATTRIBUTES
+                {
+                    Attributes = 0x00000002,
+                    Luid = privilegeId
+                }
+            };
+            IntPtr tokenHandle = IntPtr.Zero;
+            return OpenProcessToken(GetCurrentProcess(),0x0020 | 0x0008,out tokenHandle) &&
+                AdjustTokenPrivileges(tokenHandle,false,ref tokenPrivileges,1024,IntPtr.Zero,0) && 
+                CloseHandle(tokenHandle);
+        }
+    }
+}
